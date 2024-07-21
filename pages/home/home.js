3bdbaf130d8a2059f543c1cbf9286d8aab24dd4f
Deivison Gonçalves <deivisoncpmg201@gmail.com>
2024-07-21 14:17:13 -0300

    Modal relatorio/notificacoes
    criação do modal relatorio e do modal lateral de notificacoes

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,16 @@
+/* ------ Ajustando Tamanho de tela --------*/
+function ajusteTamanhoTela(){
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+ajusteTamanhoTela()
+window.addEventListener('resize', ajusteTamanhoTela);
+
+
+
+
+
+/* ------- Abrir informações de Lancamentos ------ */
 function abrirInfoLancamentos(num){
     const lancamento = document.getElementById(`lancamento${num}`)
     const btnExcluir = document.getElementById(`btnExcluir${num}`)
@@ -17,3 +30,56 @@ function abrirInfoLancamentos(num){
 
     }
 }
+
+
+
+
+
+
+/* ------ Abrir e Fechar Modal Relatorio -------- */
+function abrirRelatorio(){
+    const modal = document.getElementById('modalRelatorio')
+    const body = document.getElementById('body')
+
+    if(modal.style.display != 'flex'){
+        modal.style.display = 'flex';
+        body.style.overflow = 'hidden';
+    }else{
+        modal.style.display = 'none';
+        body.style.overflow = 'auto';
+    }
+    
+}
+
+
+
+
+/* ---------- Abir e Fechar Modal Notificações ------------*/
+
+function abrirNotificacoes(){
+    const modal = document.getElementById('modalNotificacoes')
+    const notificacoes = document.getElementById('notificacoes')
+    const body = document.getElementById('body')
+
+
+    if(notificacoes.style.display != 'block'){
+        notificacoes.style.display = 'block'
+        body.style.overflow = 'hidden'
+        modal.style.width = '100vw'
+    }else{
+        notificacoes.style.display = 'none'
+        body.style.overflow = 'auto'
+        modal.style.width = 'fit-content'
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
